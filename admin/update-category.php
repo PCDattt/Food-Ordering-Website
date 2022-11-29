3300323378fb504695afc2196e3f115499ae440d
Phù Chí Đạt <83968308+PCDattt@users.noreply.github.com>
2022-11-29 22:01:52 +0700

    Fix re-upload image bug on Category
    Fix "headers already sent" bug when re-upload image on Category

--- admin/update-category.php
+++ admin/update-category.php
@@ -95,84 +95,83 @@
             </table>
 
         </form>
+        
+        <?php 
+            if(isset($_POST['submit']))
+            {
+                $id = $_POST['id'];
+                $title = $_POST['title'];
+                $featured = $_POST['featured'];
+                $active = $_POST['active'];
+                $c_image_name = $_POST['c_image_name'];
 
+                if(isset($_FILES['image']['name']))
+                {
+                    $image_name = $_FILES['image']['name'];
+                    if($image_name != "")
+                    {
+                        //Auto rename image
+                        //Get extension
+                        $ext = end(explode('.', $image_name));
+                        //Rename image
+                        $image_name = "Food_Category_".rand(000, 999).'.'.$ext;
+
+                        $source_path = $_FILES['image']['tmp_name'];
+                        $destination_path = "../images/category/".$image_name;
+
+                        $upload = move_uploaded_file($source_path, $destination_path);
+
+                        if($upload==false)
+                        {
+                            $_SESSION['update'] = "<div class='error'>Failed to Update Image</div>";
+                            header('location:'.SITEURL.'admin/manage-category.php');
+                            die();
+                        }
+
+                        if($c_image_name != "")
+                        {
+
+                            $remove_path = "../images/category/".$c_image_name;
+
+                            $remove = unlink($remove_path);
+
+                            if(! $remove)
+                            {
+                                $_SESSION['failed-remove'] = "<div class='error'>Failed to Remove Image</div>";
+                                header('location:'.SITEURL.'admin/manage-category.php');
+                                die();
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        $image_name = $c_image_name;
+                    }
+                    
+                }
+                else
+                {
+                    $image_name = $c_image_name;
+                }
+
+                $sql2 = "UPDATE category SET title='$title', image_name='$image_name', featured='$featured', active='$active' WHERE id=$id";
+
+                $res2 = mysqli_query($conn, $sql2);
+
+                if($res2)
+                {
+                    $_SESSION['update'] = "<div class='success'>Update Category Successfully</div>";
+                    header('location:'.SITEURL.'admin/manage-category.php');
+                }
+                else
+                {
+                    $_SESSION['update'] = "<div class='error'>Failed to Update Category</div>";
+                    header('location:'.SITEURL.'admin/manage-category.php');
+                }
+            }
+    ?>
     </div>
 
 </div>
 <?php include('partials/footer.php'); ?>
-
-<?php 
-    if(isset($_POST['submit']))
-    {
-        $id = $_POST['id'];
-        $title = $_POST['title'];
-        $featured = $_POST['featured'];
-        $active = $_POST['active'];
-        $c_image_name = $_POST['c_image_name'];
-
-        if(isset($_FILES['image']['name']))
-        {
-            $image_name = $_FILES['image']['name'];
-            if($image_name != "")
-            {
-                //Auto rename image
-                //Get extension
-                $ext = end(explode('.', $image_name));
-                //Rename image
-                $image_name = "Food_Category_".rand(000, 999).'.'.$ext;
-
-                $source_path = $_FILES['image']['tmp_name'];
-                $destination_path = "../images/category/".$image_name;
-
-                $upload = move_uploaded_file($source_path, $destination_path);
-
-                if($upload==false)
-                {
-                    $_SESSION['update'] = "<div class='error'>Failed to Update Image</div>";
-                    header('location:'.SITEURL.'admin/manage-category.php');
-                    die();
-                }
-
-                if($c_image_name != "")
-                {
-
-                    $remove_path = "../images/category/".$c_image_name;
-
-                    $remove = unlink($remove_path);
-
-                    if(! $remove)
-                    {
-                        $_SESSION['failed-remove'] = "<div class='error'>Failed to Remove Image</div>";
-                        header('location:'.SITEURL.'admin/manage-category.php');
-                        die();
-                    }
-
-                }
-            }
-            else
-            {
-                $image_name = $c_image_name;
-            }
-            
-        }
-        else
-        {
-            $image_name = $c_image_name;
-        }
-
-        $sql2 = "UPDATE category SET title='$title', image_name='$image_name', featured='$featured', active='$active' WHERE id=$id";
-
-        $res2 = mysqli_query($conn, $sql2);
-
-        if($res2)
-        {
-            $_SESSION['update'] = "<div class='success'>Update Category Successfully</div>";
-            header('location:'.SITEURL.'admin/manage-category.php');
-        }
-        else
-        {
-            $_SESSION['update'] = "<div class='error'>Failed to Update Category</div>";
-            header('location:'.SITEURL.'admin/manage-category.php');
-        }
-    }
-?>
